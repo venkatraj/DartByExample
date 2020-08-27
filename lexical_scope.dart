219ff160f067823b11ed2da43f0ec1c87366c348
unknown <venkat@codinggeek.com>
2020-08-27 12:14:56 +0530

    lexical scope

---
+++ lexical_scope.dart
@@ -0,0 +1,11 @@
+void main() {
+  var functions = [];
+  for (var i = 0; i < 3; i++) {
+    functions.add(() => i);
+  }
+  functions.forEach((fn) => {print(fn())});
+}
+
+// Dart is lexical scoped.
+// But may be this page title should be
+// block scoped? Compared to `var` hoisting in js
