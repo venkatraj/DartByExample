4c75f3694a2468c7c85dc36c01ae56616dd61ff7
unknown <venkat@codinggeek.com>
2020-08-27 09:54:24 +0530

    values

---
+++ values.dart
@@ -0,0 +1,13 @@
+void main() {
+  // Append Strings
+  print('Dart' + 'lang');
+
+  // Interpolation
+  print('1+1=${1 + 1}');
+  print('7.0/3.0=${7.0 / 3.0}');
+
+  // Booleans
+  print(true && false);
+  print(false || true);
+  print(!true);
+}
