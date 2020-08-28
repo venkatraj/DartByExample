3ecbb03a28b553b1f04537cd406aec23c43c80af
unknown <venkat@codinggeek.com>
2020-08-28 16:18:23 +0530

    final

---
+++ final.dart
@@ -0,0 +1,24 @@
+// Can't run even with try...catch
+void main() {
+  final foo = 'hello';
+
+  try {
+    foo = 'goodbye';
+  } catch (e) {
+    print('error');
+  }
+
+  var pos = new Position(4);
+
+  try {
+    pos.x = 100;
+  } catch (e) {
+    print('error');
+  }
+}
+
+class Position {
+  final int x;
+  final int y;
+  Position(this.x) : y = 0;
+}
