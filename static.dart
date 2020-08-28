dd648cade7ac9f153ad2092d4ba9f2d00b305334
unknown <venkat@codinggeek.com>
2020-08-28 16:21:13 +0530

    static

---
+++ static.dart
@@ -0,0 +1,12 @@
+class Position {
+  // static function
+  static int get maxX => 100;
+
+  // static property
+  static int maxY = 200;
+}
+
+void main() {
+  print(Position.maxX);
+  print(Position.maxY);
+}
